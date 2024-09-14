cf93a53be0973c39413f235980030f9f8e7af4f3
Anujsharma285 <anujsh285@gmail.com>
2024-09-14 16:28:40 +0530

    added basic styling using Bootstrap and ejs templating

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,8 @@ const Schema=mongoose.Schema; //shorthand
 //defining the schema
 const CampgroundSchema=new Schema({
     title:String,
-    price:String,
+    image:String,
+    price:Number,
     description:String,
     location:String
 })
--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,14 @@ const seedDB= async()=>{
     //making random entries for database 
     for(let i=0;i<50;i++){
         const random1000= Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10;
        const camp= new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate fugit esse minus sequi optio. Quibusdam nulla incidunt a et iure eum enim! Id veritatis eligendi fugiat officiis, quos facere placeat!',
+            price
         })
         await camp.save();
     }
